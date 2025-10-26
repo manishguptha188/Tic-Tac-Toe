df025cac903b3dc415b0ecbf1346e68f78e576b1
Manish Guptha <manishguptha188@gmail.com>
2025-10-26 17:53:25 +0530

    Refactor Board component: update status display logic and fix calculateWinner function

--- Tic-Tac-Toe/src/App.jsx
+++ Tic-Tac-Toe/src/App.jsx
@@ -13,6 +13,14 @@ export default function Board() {
    const [isNext, setisNext ] = useState(true)
    const [squares, setSquares ] = useState(Array(9).fill(null));
    
+   const winner = calculateWinner(squares);
+   let status;
+   if (winner) {
+      status = "Winner is : " + winner;
+   }else{
+      status = "Next Player is : " + (isNext ? "X" : "O");
+   }
+
    function calculateWinner(squares) {
    const lines = [
       [0, 1, 2],
@@ -25,9 +33,9 @@ export default function Board() {
       [2, 4, 6],
    ]
    for ( let i = 0; i < lines.length; i++) {
-      count [a, b, c] = lines[i];
+      const [a, b, c] = lines[i];
       if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
-         return[a];
+         return squares[a];
       }
    }
 }
@@ -48,6 +56,7 @@ export default function Board() {
     }
     
     return (<>
+    <div className="status">{status}</div>
      <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => {handleClick(0)}}/>
         <Square value={squares[1]} onSquareClick={() => {handleClick(1)}}/>
