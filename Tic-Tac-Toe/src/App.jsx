395b66dff97983bab4970f22535b3e791d898174
Manish Guptha <manishguptha188@gmail.com>
2025-10-26 17:38:50 +0530

    Refactor Board component: implement calculateWinner function to determine game outcome

--- Tic-Tac-Toe/src/App.jsx
+++ Tic-Tac-Toe/src/App.jsx
@@ -10,10 +10,31 @@ function Square({ value, onSquareClick }) {
 
 
 export default function Board() {
-    const [isNext, setisNext ] = useState(true)
-    const [squares, setSquares ] = useState(Array(9).fill(null));
-    
-    function handleClick(i) {
+   const [isNext, setisNext ] = useState(true)
+   const [squares, setSquares ] = useState(Array(9).fill(null));
+   
+   function calculateWinner(squares) {
+   const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+   ]
+   for ( let i = 0; i < lines.length; i++) {
+      count [a, b, c] = lines[i];
+      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+         return[a];
+      }
+   }
+}
+
+   function handleClick(i) {
+      if (squares[i] || calculateWinner(squares)) return;
+
       const nextSquares = squares.slice();
 
        if (isNext) {
@@ -46,4 +67,3 @@ export default function Board() {
         </>
     );
 }
-
